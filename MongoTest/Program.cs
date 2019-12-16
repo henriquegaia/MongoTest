17bdf401714ce8face30915c010767155e9fd5b8
henrique <henrique.gaia@gmail.com>
2019-12-16 15:09:36 +0100

    init

---
+++ MongoTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace mongodb_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MongoCRUD db = new MongoCRUD("AddressBook");
+            Console.ReadLine();
+        }
+    }
+
+    public class MongoCRUD
+    {
+        private IMongoDatabase db;
+        
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database); ;
+        }
+    }
+
+}
