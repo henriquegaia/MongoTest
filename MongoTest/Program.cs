c1bed65d55181e8a8c5799f18d7874a5a4d535e9
henrique <henrique.gaia@gmail.com>
2019-12-18 13:34:50 +0100

    Add address and get all records

--- MongoTest/Program.cs
+++ MongoTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
@@ -8,25 +10,50 @@ namespace mongodb_tests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            MongoCRUD db = new MongoCRUD("AddressBook");
-            db.InsertRecord("Users", new Person { FirstName = "Jeff", LastName = "Bezos" }); ;
+            Console.WriteLine("starting ...");
+            string database = "AddressBook";
+            MongoCRUD db = new MongoCRUD(database);
+            var address = new Address { Street = "Gold", City = "PH", State = null, ZipCode = "1234" };
+
+            db.InsertRecord("Users", new Person
+            {
+                FirstName = "Jeff",
+                LastName = "Bezos",
+                PrimaryAddress = address
+            });
+
+            var allRecords = db.LoadRecords<Person>("Users");
+
+            foreach (var record in allRecords)
+            {
+                Console.WriteLine(record.FirstName);
+            }
+
             Console.ReadLine();
         }
     }
 
     public class Person
     {
-        [BsonId]
-        public Guid Id { get; set; }
+        //[BsonId]
+        //public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public Address PrimaryAddress { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
     }
 
     public class MongoCRUD
     {
         private IMongoDatabase db;
-        
+
         public MongoCRUD(string database)
         {
             var client = new MongoClient();
@@ -37,6 +64,12 @@ namespace mongodb_tests
         {
             var collection = db.GetCollection<T>(table);
             collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
         }
     }
 
